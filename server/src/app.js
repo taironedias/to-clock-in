8fb2bf1f600ed650fd83af0151c2d6554e47cb8e
taironecdias <taironecdias@gmail.com>
2020-06-25 22:06:00 -0300

    definindo o formato json do request e iniciando o arquivo de rotas

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json(true);
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => {
     console.log('server running port 3333');
