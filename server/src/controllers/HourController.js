a8653586d3a2e0d80c789dd9ed996af390da8140
taironecdias <taironecdias@gmail.com>
2020-06-26 22:59:28 -0300

    adicionando o controller das horas de trabalho com a função de create

---
+++ server/src/controllers/HourController.js
@@ -0,0 +1,16 @@
+import connection from '../database/connection';
+
+export default {
+    async create(req, res) {
+        const { punch_clock } = req.body;
+        const user_id = req.headers.authorization;
+
+        const [id] = await connection('hours').insert({
+            user_id,
+            punch_clock
+        });
+
+        return res.json({ id });
+    },
+
+}
