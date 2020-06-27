12d48eacf27e5129b1020eca3942587b240aac96
taironecdias <taironecdias@gmail.com>
2020-06-26 22:56:48 -0300

    adicionando as instruções de update de usuário

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -27,5 +27,31 @@ export default {
     async index(req, res) {
         const users = await connection('users').select('*');
         return res.json(users);
+    },
+
+    async update(req, res) {
+        const { id } = req.params;
+        const user_id = req.headers.authorization;
+
+        if (id !== user_id) {
+            return res.status(401).json({ error: 'Operation not permitted.' });
+        }
+
+        const { login, password, name, email } = req.body;
+
+        const user = await connection('users')
+            .where({ id: id })
+            .update({
+                login,
+                password,
+                name,
+                email
+            }, ['id', 'login', 'password', 'name', 'email']);
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found for update.' });
+        }
+
+        return res.status(204).send();
     }
 }
